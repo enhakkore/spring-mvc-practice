b29c5a55e9bfabeeab747b45325f7d6865e75a58
enhakkore <ahn.ephraim@gmail.com>
2019-12-04 15:06:35 +0900

    Add viewResolver

--- src/main/java/spring/mvc/practice/config/ChimneyConfiguration.java
+++ src/main/java/spring/mvc/practice/config/ChimneyConfiguration.java
@@ -1,9 +1,19 @@
 package spring.mvc.practice.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan("spring.mvc.practice")
 public class ChimneyConfiguration {
+
+    @Bean
+    public InternalResourceViewResolver internalResourceViewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
 }
